2c9717d51b5183f2fdab2873cdc811b8263242c7
prashanthnaid <168257081+prashanthnaid@users.noreply.github.com>
2024-10-29 13:33:48 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "manireddit" # Replace with your actual S3 bucket name
+    bucket = "pet1" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "us-east-2"
+    region = "ap-south-1"
   }
 }
